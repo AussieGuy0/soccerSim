1619af2436821bba9286ca85361c83233bbc725f
AussieGuy0 <anthony.bruno196@gmail.com>
2016-12-28 22:02:54 +1030

    Xml to Team almost finished

--- src/main/java/me/anthonybruno/soccerSim/models/Team.java
+++ src/main/java/me/anthonybruno/soccerSim/models/Team.java
@@ -16,10 +16,19 @@ public class Team {
     private final HalfAttributes secondHalfAttributes;
     private final String formation;
     private final String strategy;
-    private final Stats stats = new Stats();
 
+    private final Stats stats = new Stats();
     private final ArrayList<Goalie> goalies = new ArrayList<>();
     private final ArrayList<Player> players = new ArrayList<>();
+
+    public Team(String name, int shotsGoal, String formation, String strategy, HalfAttributes firstHalfAttributes, HalfAttributes secondHalfAttributes) {
+        this.name = name;
+        this.shotsGoal = shotsGoal;
+        this.formation = formation;
+        this.strategy = strategy;
+        this.firstHalfAttributes = firstHalfAttributes;
+        this.secondHalfAttributes = secondHalfAttributes;
+    }
 
     public Team(String name, int firstHalfAttempts, int secondHalfAttempts, int shotsGoal, int firstHalfDefenseAttempts, int secondHalfDefenseAttempts, int firstHalfDefensiveShotOnGoal, int secondHalfDefensiveShotOnGoal, String formation, @SuppressWarnings("SameParameterValue") String strategy) {
         this.firstHalfAttributes = new HalfAttributes(firstHalfAttempts, firstHalfDefenseAttempts, firstHalfDefensiveShotOnGoal);
@@ -114,7 +123,43 @@ public class Team {
         return players.get(0);
     }
 
-    public class CarBuilder {
+    public static class Builder {
+        private String name;
+        private int shotsGoal;
+        private HalfAttributes firstHalfAttributes;
+        private HalfAttributes secondHalfAttributes;
+        private String formation;
+        private String strategy;
 
+        public void name(String name) {
+            this.name = name;
+        }
+
+        public void shotsGoal(int shotsGoal) {
+            this.shotsGoal = shotsGoal;
+        }
+
+
+        public void firstHalfAttributes(HalfAttributes firstHalfAttributes) {
+            this.firstHalfAttributes = firstHalfAttributes;
+        }
+
+
+        public void secondHalfAttributes(HalfAttributes secondHalfAttributes) {
+            this.secondHalfAttributes = secondHalfAttributes;
+        }
+
+
+        public void formation(String formation) {
+            this.formation = formation;
+        }
+
+        public void strategy(String strategy) {
+            this.strategy = strategy;
+        }
+
+        public Team build() {
+            return new Team(name, shotsGoal, formation, strategy, firstHalfAttributes, secondHalfAttributes);
+        }
     }
 }
--- src/main/java/me/anthonybruno/soccerSim/reader/XmlParser.java
+++ src/main/java/me/anthonybruno/soccerSim/reader/XmlParser.java
@@ -1,10 +1,13 @@
 package me.anthonybruno.soccerSim.reader;
 
+import me.anthonybruno.soccerSim.models.HalfAttributes;
 import me.anthonybruno.soccerSim.models.Team;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
@@ -13,6 +16,17 @@ import java.io.IOException;
  */
 public class XmlParser {
 
+    private final static String TEAM_NAME = "name";
+    private final static String GOAL_RATING = "goalRating";
+    private final static String FORMATION = "formation";
+    private final static String STRATEGY = "strategy";
+    private final static String PLAYERS = "players";
+
+    private final static String HALF_ATTRIBUTES = "halfStats";
+    private final static String ATTEMPTS = "attempts";
+    private final static String DEFENSIVE_ATTEMPTS = "defensiveAttempts";
+    private final static String DEFENSIVE_SHOTS_ON_GOAL= "defensiveShotsOnGoal";
+
     private File file;
 
     public XmlParser(File file) {
@@ -20,16 +34,6 @@ public class XmlParser {
     }
 
     public Team parseXmlIntoTeam() {
-//        JAXBContext jaxbContext = null;
-//        try {
-//            jaxbContext = JAXBContext.newInstance(Team.class);
-//            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-//            Team team = (Team) jaxbUnmarshaller.unmarshal(file);
-//            return team;
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
-
         DocumentBuilderFactory factory =
                 DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = null;
@@ -49,14 +53,75 @@ public class XmlParser {
         }
         Element root = document.getDocumentElement();
         NodeList childNodes = root.getChildNodes();
+        Team.Builder teamBuilder = new Team.Builder();
+        int halfStatsNum = 1;
 
         for (int i = 0; i < childNodes.getLength(); i++) {
             Node current = childNodes.item(i);
             if (!(current instanceof Text)) {
-                System.out.println(current.getTextContent());
+                String tagName = current.getNodeName();
+                String tagValue = current.getTextContent();
+
+                switch (tagName) {
+                    case TEAM_NAME:
+                        teamBuilder.name(tagValue);
+                        break;
+                    case GOAL_RATING:
+                        teamBuilder.shotsGoal(Integer.parseInt(tagValue));
+                        break;
+                    case FORMATION:
+                        teamBuilder.formation(tagValue);
+                        break;
+                    case STRATEGY:
+                        teamBuilder.strategy(tagValue);
+                        break;
+                    case HALF_ATTRIBUTES:
+                        handleHalfStats(teamBuilder, current, halfStatsNum++);
+                        break;
+                    case PLAYERS:
+                        handlePlayers(teamBuilder, current);
+                        break;
+                }
             }
         }
-        return null;
+        return teamBuilder.build();
+    }
+
+    private void handlePlayers(Team.Builder teamBuilder, Node playersNode) {
+        //TODO: THIS
+    }
+
+    private void handleHalfStats(Team.Builder teamBuilder, Node halfNode, int halfNum) {
+        int attempts = 0;
+        int defensiveAttempts = 0;
+        int defensiveShotsOnGoal = 0;
+        for (int i = 0; i < halfNode.getChildNodes().getLength(); i++) {
+            Node current = halfNode.getChildNodes().item(i);
+            if (!(halfNode instanceof Text)) {
+                String tagName = current.getNodeName();
+                String tagValue = current.getTextContent();
+
+                switch (tagName) {
+                    case ATTEMPTS:
+                        attempts = Integer.parseInt(tagValue);
+                        break;
+                    case DEFENSIVE_ATTEMPTS:
+                        defensiveAttempts = Integer.parseInt(tagValue);
+                        break;
+                    case DEFENSIVE_SHOTS_ON_GOAL:
+                        defensiveShotsOnGoal = Integer.parseInt(tagValue);
+                        break;
+                }
+
+            }
+        }
+
+        HalfAttributes halfAttributes = new HalfAttributes(attempts, defensiveAttempts, defensiveShotsOnGoal);
+        if (halfNum == 1) {
+            teamBuilder.firstHalfAttributes(halfAttributes);
+        } else {
+            teamBuilder.secondHalfAttributes(halfAttributes);
+        }
     }
 
     public static void main(String[] args) {
